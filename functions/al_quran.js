14030b554efa70c241f9206b191fb6a9cc28b672
Lim Kha Shing <kslim5703@gmail.com>
2020-09-11 23:40:28 +0800

    Refactor al_quran.js

--- functions/al_quran.js
+++ functions/al_quran.js
@@ -1,16 +1,16 @@
 const functions = require('firebase-functions');
 const algoliaSearch = require('algoliasearch');
-const alQuranIndexName= 'al_quran'
 
 const APP_ID = functions.config().algolia_dev.app;
 const ADMIN_KEY = functions.config().algolia_dev.key;
 const algoliaClient = algoliaSearch(APP_ID, ADMIN_KEY);
+const stagingAlQuranName= 'dev_al_quran'
+const alQuranIndex = algoliaClient.initIndex(stagingAlQuranName);
 
-const alQuranIndex = algoliaClient.initIndex(alQuranIndexName);
-
+const firebaseAlQuranCollectionName= 'al_quran'
 const HONG_KONG_REGION = "asia-east2"
 
-exports.addToAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(alQuranIndexName+'/{AlQuranChapters}')
+exports.addToAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(firebaseAlQuranCollectionName+'/{AlQuranChapters}')
     .onCreate(snapshot => {
         const data = snapshot.data();
         const objectID = snapshot.id
@@ -18,7 +18,7 @@ exports.addToAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.documen
     }
 );
 
-exports.updateAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(alQuranIndexName+'/{AlQuranChapters}')
+exports.updateAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(firebaseAlQuranCollectionName+'/{AlQuranChapters}')
     .onUpdate((change) => {
         const newData = change.after.data()
         const objectID = change.after.id
@@ -26,7 +26,7 @@ exports.updateAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.docume
     }
 );
 
-exports.deleteFromAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(alQuranIndexName+'/{AlQuranChapters}')
+exports.deleteFromAlQuranIndex = functions.region(HONG_KONG_REGION).firestore.document(firebaseAlQuranCollectionName+'/{AlQuranChapters}')
     .onDelete(snapshot => {
         alQuranIndex.deleteObject(snapshot.id)
     }
